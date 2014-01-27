daa76f8ad67e77dc5a0184eda5d1839dd0e73536
Matt Buland <gik0geck0@gmail.com>
2014-01-27 11:00:51 -0700

    Fix tokenization to be more sophisticated then just 'words'ing. Lack of spaces is now acceptable

--- parser.hs
+++ parser.hs
@@ -3,7 +3,6 @@ module Lexer where
 import Data.Maybe
 import Debug.Trace
 import Tokenizer
-import Text.Regex.Posix
 
 type Parser a = Tokens -> Maybe (a, Tokens)
 
@@ -11,8 +10,8 @@ data Program = Program Declarations Statements deriving Show
 
 -- Note: It's implied here that there MUST be an initialization. This is because I don't know how to do Empty binding
 data Declaration =
-      Finit Identifier
-    | Iinit Identifier deriving Show
+      FDcl Identifier
+    | IDcl Identifier deriving Show
 data Declarations =
       Declarations Declaration Declarations
     | NoDeclarations deriving Show
@@ -35,7 +34,7 @@ data Value =
     | FValue Float
     | IValue Int deriving Show
 
-data Identifier = Identifier String deriving Show
+data Identifier = Identifier Char deriving Show
 
 data Operator =
       Plus
@@ -61,7 +60,7 @@ declarations (t:ts) =
     let decl = declaration (t:ts)
         moreDecls = declarations $ snd $ fromJust decl
     -- Predict if this will be a valid declaration
-    in if t == "f" || t == "i" then
+    in if t == Finit || t == Iinit then
         if isJust decl then
             if isJust moreDecls then Just (Declarations (fst $ fromJust decl) (fst $ fromJust moreDecls), snd $ fromJust moreDecls)
                 else trace "Error in a deeper declaration" Nothing
@@ -74,8 +73,8 @@ declaration (_:[]) = Nothing
 declaration (t:tkns) =
     let ident = identifier tkns
     in if isJust ident then
-        if t == "f" then Just (Finit $ fst $ fromJust ident, snd $ fromJust ident)
-        else if t == "i" then Just (Iinit $ fst $ fromJust ident, snd $ fromJust ident)
+        if t == Finit then Just (FDcl $ fst $ fromJust ident, snd $ fromJust ident)
+        else if t == Iinit then Just (IDcl $ fst $ fromJust ident, snd $ fromJust ident)
         else trace "Invalid type instantiator" Nothing
     -- NOTE: We don't want a trace here. We use the failing from this to tell us that a declaration is not going on.
     else Nothing
@@ -85,11 +84,12 @@ statements [] = Just (NoStatements, [])
 statements (t:[]) = Just (NoStatements, t:[])
 statements (t:ts) =
     -- Predict if there are more statements
-    if t == "p" || (isJust $ identifier (t:[]))
-        then let stmt = statement (t:ts)
+    if t == PrintTok || (isJust $ identifier (t:[]))
+        then
+        let stmt = statement (t:ts)
+            moreStmts = statements $ snd $ fromJust stmt
         in if isJust stmt then
-            let moreStmts = statements $ snd $ fromJust stmt
-            in if isJust moreStmts
+            if isJust moreStmts
                 then Just (Statements (fst $ fromJust stmt) (fst $ fromJust moreStmts), snd $ fromJust moreStmts)
             else trace "Stmts::MoreStatements has Errored" Nothing
         else trace "Stmts::oneStatement has Errored" Nothing
@@ -100,22 +100,26 @@ statement :: Parser Statement
 statement [] = trace "Ran out of tokens" Nothing
 statement (_:[]) = trace "Statements require more than 1 token" Nothing
 statement (t:ts) =
-    if t == "p" then
-        let ident = identifier ts
-        in if isJust ident then Just (Print $ fst $ fromJust ident, snd $ fromJust ident)
-            else Nothing
-    else
-        let saveTo = identifier (t:[])
-        in if isJust saveTo && head ts == "=" then
-            let val = value $ tail ts
-            in if isJust val then
-                if length ts > 1 then
-                    let expr = expression $ tail $ tail ts
-                    in if isJust expr then Just (Assignment (fst $ fromJust saveTo) (fst $ fromJust val) (fst $ fromJust expr), snd $ fromJust expr)
-                    else trace "Statement:: Following expression has failed" Nothing
-                else trace "Statement:: Not enough tokens for '=' and a value" Nothing
-            else trace "Statement:: No value found" Nothing
-        else trace "Statement:: Expected an identifier followed by an equals" Nothing
+    case t of
+        PrintTok ->
+            let ident = identifier ts
+            in if isJust ident then Just (Print $ fst $ fromJust ident, snd $ fromJust ident)
+                else Nothing
+        _        ->
+            let saveTo = identifier (t:[])
+                expr = expression $ tail $ tail ts
+                val = value $ tail ts
+            in case head ts of
+                AssignTok ->
+                    if isJust saveTo then
+                        if isJust val then
+                            if length ts > 1 then
+                                if isJust expr then Just (Assignment (fst $ fromJust saveTo) (fst $ fromJust val) (fst $ fromJust expr), snd $ fromJust expr)
+                                else trace "Statement:: Following expression has failed" Nothing
+                            else trace "Statement:: Not enough tokens for '=' and a value" Nothing
+                        else trace "Statement:: No value found" Nothing
+                    else trace "Statement:: No identifier to save to" Nothing
+                _  -> trace "Statement:: Expected an identifier followed by an equals" Nothing
 
 expression :: Parser Expression
 expression [] = Just (NoExpression, [])
@@ -134,20 +138,32 @@ expression (t:ts) =
 operator :: Parser Operator
 operator [] = Nothing
 operator (t:ts) =
-    if t == "+" then Just (Plus, ts)
-    else if t == "-" then Just (Minus, ts)
-    else Nothing
+    case t of
+        PlusTok  -> Just (Plus, ts)
+        MinusTok -> Just (Minus, ts)
+        _        -> trace ("Unknown operator: " ++ show t) Nothing
+    -- if t == PlusTok then Just (Plus, ts)
+    -- else if t == MinusTok then Just (Minus, ts)
+    -- else Nothing
 
 value :: Parser Value
 value [] = Nothing
 value (t:ts) =
-    if t =~ "^[0-9]+$" then Just (IValue $ read t, ts)
-    else if t =~ "^[+-]?([0-9]+\\.[0-9]*|[0-9]*\\.[0-9]+)([eE][+-]?([0-9]+\\.[0-9]*|[0-9]*\\.[0-9]+))?$" then Just (FValue $ read t, ts)
-    else let ident = identifier (t:[])
-        in if not $ isNothing ident then Just (SValue $ fst $ fromJust ident, ts) else Nothing
+    case t of
+        ITok i -> Just (IValue i, ts)
+        FTok f -> Just (FValue f, ts)
+        _      -> let ident = identifier (t:[])
+                    in if not $ isNothing ident then Just (SValue $ fst $ fromJust ident, ts) else Nothing
+    -- if t =~ "^[0-9]+$" then Just (IValue $ read t, ts)
+    -- else if t =~ "^[+-]?([0-9]+\\.[0-9]*|[0-9]*\\.[0-9]+)([eE][+-]?([0-9]+\\.[0-9]*|[0-9]*\\.[0-9]+))?$" then Just (FValue $ read t, ts)
+    --else let ident = identifier (t:[])
+    --    in if not $ isNothing ident then Just (SValue $ fst $ fromJust ident, ts) else Nothing
 
 identifier :: Parser Identifier
 identifier [] = Nothing
 identifier (t:ts) =
     -- An identifier must NOT be the single letter: i,f,p, but any identifier that starts with it MUST have more than one character
-    if t =~ "^[a-eghj-oq-zA-Z]|[a-zA-Z][a-zA-Z]+$" then Just (Identifier t, ts) else Nothing
+    case t of
+        IdentifierTok it -> Just (Identifier it, ts)
+        _               -> trace ("Expected an identifier, but got " ++ show t) Nothing
+    -- if t =~ "^[a-eghj-oq-zA-Z]|[a-zA-Z][a-zA-Z]+$" then Just (Identifier t, ts) else Nothing
--- tokenizer.hs
+++ tokenizer.hs
@@ -1,5 +1,6 @@
 module Tokenizer where
 
+import Debug.Trace
 import Text.Regex.Posix
 
 data Token =
@@ -7,13 +8,59 @@ data Token =
     | ITok Int
     | Finit
     | Iinit
-    | Identifier Char
-    deriving Show
+    | PrintTok
+    | PlusTok
+    | MinusTok
+    | AssignTok
+    | IdentifierTok Char
+    deriving (Show, Eq)
 
 type Tokens = [Token]
 
+type PartialInt = (String, String)
+type PartialFloat = (String, String)
+
 tokenize :: String -> Tokens
+tokenize [] = []
 tokenize (c:cs) =
-    if c == "f" then Finit
-    else if c == "i" then Iinit
-    else if c =~ "^[a-eghj-oq-zA-Z]" then  (Identifier c):tokenize cs
+    if c == 'f' then Finit:(tokenize cs)
+    else if c == 'i' then Iinit:(tokenize cs)
+    else if c == 'p' then PrintTok:(tokenize cs)
+    else if c == '+' then PlusTok:(tokenize cs)
+    else if c == '-' then MinusTok:(tokenize cs)
+    else if c == '=' then AssignTok:(tokenize cs)
+    else if (c:[]) =~ "^[a-eghj-oq-zA-Z]" then  (IdentifierTok c):tokenize cs
+    else if (c:cs) =~ "^[0-9]+\\.[0-9]+" then
+        let (ftok, ts) = buildFloat $ fTokenize (c:cs)
+        in (FTok ftok):(tokenize ts)
+    else if (c:cs) =~ "^[0-9]+" then
+        let (itok, ts) = buildInt $ iTokenize (c:cs)
+        in (ITok itok):(tokenize ts)
+    else if c == ' ' then tokenize cs else trace ("Skipping invalid character: " ++ c:[]) $ tokenize cs
+
+fTokenize :: String -> (String, String)
+fTokenize [] = ([],[])
+fTokenize (c:[]) = (c:[],[])
+fTokenize (c:cs) =
+    if (c:[]) =~ "[0-9.]" then floatBuilder c (fTokenize cs)
+    else ([], c:cs)
+
+floatBuilder :: Char -> (String, String) -> (String, String)
+floatBuilder c (cs, remaining) = (c:cs, remaining)
+
+buildFloat :: PartialFloat -> (Float, String)
+buildFloat (fstr, cs) = (read fstr, cs)
+
+
+iTokenize :: String -> (String, String)
+iTokenize [] = ([],[])
+iTokenize (c:[]) = (c:[],[])
+iTokenize (c:cs) =
+    if (c:[]) =~ "[0-9]" then intBuilder c (iTokenize cs)
+    else ([], c:cs)
+
+intBuilder :: Char -> (String, String) -> PartialInt
+intBuilder c (cs, remaining) = (c:cs, remaining)
+
+buildInt :: PartialInt -> (Int, String)
+buildInt (istr, cs) = (read istr, cs)
