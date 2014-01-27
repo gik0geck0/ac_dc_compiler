7fa82e58dd31e95a677438b07109aa4a47526763
Matt Buland <gik0geck0@gmail.com>
2014-01-27 11:09:02 -0700

    Switch to using Haskeline for input

--- main.hs
+++ main.hs
@@ -1,13 +1,25 @@
 
 import Debug.Trace
+import System.Console.Haskeline
+
 import Lexer
 import Tokenizer
 
 main :: IO ()
-main = do
-    s <- getLine
-    putStrLn $ show $ parse $ tracer $ tokenize s
-    main
+-- main = do
+--     s <- getLine
+--     putStrLn $ show $ parse $ tracer $ tokenize s
+--     main
+main = runInputT defaultSettings loop
+    where
+        loop :: InputT IO ()
+        loop = do
+            minput <- getInputLine "> "
+            case minput of
+                Nothing     -> return ()
+                Just "quit" -> return ()
+                Just input  -> do outputStrLn $ show $ parse $ tokenize input
+                                  loop
 
 tracer :: (Show a) => a -> a
 tracer a = trace ("Tokens: " ++ show a) a
