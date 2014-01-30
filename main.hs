94a816787f10425f4117710d24b618f6e9ae00d4
Matt Buland <gik0geck0@gmail.com>
2014-01-29 17:13:57 -0700

    Add a test AC file and remove prompt line (allows for file input and direct piping to dc)

--- main.hs
+++ main.hs
@@ -16,17 +16,18 @@ main = runInputT defaultSettings loop
     where
         loop :: InputT IO ()
         loop = do
-            minput <- getInputLine "> "
+            minput <- getInputLine ""
             case minput of
                 Nothing     -> return ()
                 Just "quit" -> return ()
                 Just input  -> 
                     let parseTree = parse $ tokenize input
-                    in do   outputStrLn $ show parseTree
-                            if isJust parseTree then
-                                outputStrLn $ genDC $ fromJust parseTree
-                            else outputStr ""
-                            loop
+                    in do
+                        -- outputStrLn $ show parseTree
+                        if isJust parseTree then
+                            outputStrLn $ genDC $ fromJust parseTree
+                        else outputStr ""
+                        loop
 
 tracer :: (Show a) => a -> a
 tracer a = trace ("Tokens: " ++ show a) a
